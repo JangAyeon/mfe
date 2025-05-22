ee2045be9ca66944179ac230332d83027a956ffd
JangAyeon <mediarkayeon@gmail.com>
2025-05-22 14:03:00 +0900

    set prefix for CSS

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -2,13 +2,23 @@ import React from "react";
 import MarketingApp from "./components/MarketingApp";
 import Header from "./components/Header";
 import { BrowserRouter } from "react-router-dom";
+import {
+  StylesProvider,
+  createGenerateClassName,
+} from "@material-ui/core/styles";
+
+const generateClassName = createGenerateClassName({
+  productionPrefix: "co", // 컨테이너 앱은 'co' 접두사 사용
+});
 export default () => {
   return (
     <BrowserRouter>
-      <div>
-        <Header />
-        <MarketingApp />
-      </div>
+      <StylesProvider generateClassName={generateClassName}>
+        <div>
+          <Header />
+          <MarketingApp />
+        </div>
+      </StylesProvider>
     </BrowserRouter>
   );
 };
